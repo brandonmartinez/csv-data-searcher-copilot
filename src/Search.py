7318a00f8464a5cfc4f8c311169b0f3e50ecf0bf
Brandon Martinez <me@brandonmartinez.com>
2024-07-10 18:54:44 +0000

    Switched to chat prompt

--- src/Search.py
+++ src/Search.py
@@ -1,6 +1,5 @@
 import os
-from langchain_core.prompts import PromptTemplate
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain.globals import set_debug
 from langchain_openai import AzureChatOpenAI
@@ -25,21 +24,22 @@ class RecordSearcher():
         self.output_parser = StrOutputParser()
 
     def search(self, prompt: str, record: str) -> str:
-        record_prompt = PromptTemplate.from_template(
-            """You are an expert at taking a record from a data set and searching the data for a specific scenario.
+        record_prompt = ChatPromptTemplate.from_messages([
+            (
+                "system",
+                """You are an expert at taking a record from a data set and searching the data for a specific scenario.
 With the given record, verify if it contains relevant information based on the given user prompt.
 If the record contains relevant information, provide a summary of the information found.
 Do not include any prompts, call outs, or additional information that is not directly from the summary.
 If the record doesn't contain relevant information, provide only "N/A" as the response; no additional context or information.
 
-USER PROMPT:
-{prompt}
-
 RECORD:
 {record}
-
-SUMMARY:
-""")
+"""
+            ),
+            ("human", "{prompt}"),
+        ]
+        )
 
         record_summary_chain = (
             record_prompt | self.model | self.output_parser
